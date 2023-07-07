2da1a701d244893d2363eae0869f48753887e1f0
jorge-rufino <jorge.rufino1986@gmail.com>
2023-07-07 15:09:03 -0300

    Endpoints Listar e Buscar produto

--- src/main/java/com/rufino/product/controllers/ProductController.java
+++ src/main/java/com/rufino/product/controllers/ProductController.java
@@ -1,16 +1,23 @@
 package com.rufino.product.controllers;
 
-import com.rufino.product.dtos.ProductRecordDto;
-import com.rufino.product.models.ProductModel;
-import com.rufino.product.services.ProductService;
-import jakarta.validation.Valid;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.rufino.product.dtos.ProductRecordDto;
+import com.rufino.product.models.ProductModel;
+import com.rufino.product.services.ProductService;
+
+import jakarta.validation.Valid;
 
 @RestController
 public class ProductController {
@@ -19,9 +26,25 @@ public class ProductController {
     private ProductService productService;
 
     @PostMapping("/products")
-    public ResponseEntity<ProductModel> save(@RequestBody @Valid ProductRecordDto productRecordDto){
+    public ResponseEntity<ProductModel> salvar(@RequestBody @Valid ProductRecordDto productRecordDto){
         var productModel = new ProductModel();
         BeanUtils.copyProperties(productRecordDto, productModel);
         return ResponseEntity.status(HttpStatus.CREATED).body(productService.save(productModel));
+    }
+    
+    @GetMapping("/products")
+    public ResponseEntity<List<ProductModel>> listar(){
+    	return ResponseEntity.status(HttpStatus.OK).body(productService.listar());
+    }
+    
+    @GetMapping("/products/{idProduct}")
+    public ResponseEntity<Object> buscar(@PathVariable UUID idProduct){
+    	ProductModel productModel = productService.buscar(idProduct);
+    	System.out.println(productModel);
+    	if(productModel != null) {
+    		return ResponseEntity.status(HttpStatus.OK).body(productModel);
+    	} else {
+    		return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Produto não encontrado!");
+    	}
     }
 }
--- src/main/java/com/rufino/product/services/ProductService.java
+++ src/main/java/com/rufino/product/services/ProductService.java
@@ -1,9 +1,13 @@
 package com.rufino.product.services;
 
-import com.rufino.product.models.ProductModel;
-import com.rufino.product.repositories.ProductRepository;
+import java.util.List;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.rufino.product.models.ProductModel;
+import com.rufino.product.repositories.ProductRepository;
 
 @Service
 public class ProductService {
@@ -13,5 +17,13 @@ public class ProductService {
 
     public ProductModel save(ProductModel productModel){
         return productRepository.save(productModel);
+    }
+    
+    public List<ProductModel> listar(){
+    	return productRepository.findAll();
+    }
+    
+    public ProductModel buscar(UUID idProduct) {
+    	return productRepository.findById(idProduct).orElse(null);
     }
 }
